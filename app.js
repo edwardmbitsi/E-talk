3f4b766e72b870425eb9ff4b507f0f63b7d2ca4e
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-10-14 17:37:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ function talk(){
 var know = {
 "Who are you" : "Hello,I am E Talk",
 "How are you" : "I am Good :)",
-"I need some to talk to and also some advice" : "Reach us on our email mbitsilikesyou@gmail.com",
+"I need someone to talk to" :  "Reach us on our email mbitsilikesyou@gmail.com, we answer in minutes",
+"I need advice" :  "Reach us on our email mbitsilikesyou@gmail.com, we answer in minutes", 
+"I need some to talk to and also some advice" : "Reach us on our email mbitsilikesyou@gmail.com, we answer in minutes",
 "What is this all about" : "This is an initiative to improve the mental health of individuals by offering a listening ear and expert advice on daily matters",
 "Okay" : "Thank You So Much, Dont Hesitate To Reach Out ",
 "Bye" : "Bye!.."
