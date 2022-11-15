78cf8a7f8aeae5e3e5720cfa556f4dbdf9abf771
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-11-15 09:31:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function talk(){
 var know = {
 "Who are you" : "Hello,I am E Talk",
+"Hi" : "Hello :)",  
 "Hello" : "Hi",  
 "How are you" : "I am Good :)",
 "I need someone to talk to" :  "Reach us on our email mbitsilikesyou@gmail.com, we answer in minutes",
